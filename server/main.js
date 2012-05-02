b0751cce9aac9677bc18f128685537cca2182b76
Renato Riccieri <renato.riccieri@gmail.com>
2012-05-02 09:03:45 -0300

    Documentação para o arquivo main.js

--- server/main.js
+++ server/main.js
@@ -1,9 +1,24 @@
+// Arquivo "principal" do servidor. Pensado para ser usado de forma análoga ao
+// método "main" do Java.
+// É aqui que o Server (do arquivo udp_server.js), o Message (do message.js) e
+// o Channel (channel.js) são colocados pra trabalhar juntos, fazendo emergir
+// o comportamento final do sistema.
+
 var Server  = require("./udp_server").Server;
+
+// O objeto 'channels' é apenas um conjunto de helpers para facilitar o
+// gerenciamento de vários canais. Os seus métodos ('byName' e 'toString')
+// estão declarados no arquivo channels.js
 var channels = require("./channels");
 
 var SERVER_PORT = 12345;
 
 var server = new Server("127.0.0.1", SERVER_PORT);
+
+// Esta função é executada toda vez que o servidor emite o evento "message".
+// É aqui que é feita a checagem do tipo da mensagem (pedido de listagem,
+// publicação ou inscrição), e são tomadas as devidas ações (inscrever o
+// cliente ao canal, publicar sua mensagem etc).
 
 server.on("message", function(message, client) {
   if(message.isListRequest()) {
