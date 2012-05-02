a8377ac6c6fe2f03e78b554c590d590fbcfa1854
Renato Riccieri <renato.riccieri@gmail.com>
2012-05-02 08:43:40 -0300

    Remove EventEmitter from channel

--- server/channel.js
+++ server/channel.js
@@ -1,31 +1,28 @@
-var EventEmitter = require("events").EventEmitter;
-
-var prototype = new EventEmitter();
-
-prototype.publish = function(message) {
-  var formattedMessage = this.name + ": " + message.content;
-
-  this.subscribers.forEach(function(subscriber) {
-    if(subscriber !== message.publisher) {
-      subscriber.send(formattedMessage);
-    }
-  });
-
-  return this;
-};
-
-prototype.addSubscriber = function(subscriber) {
-  this.subscribers.push(subscriber);
-};
-
-prototype.hasSubscriber = function(subscriber) {
-  return this.subscribers.indexOf(subscriber) > 0;
-};
-
 var Channel = function(name) {
   this.name        = name;
   this.subscribers = [];
 };
 
-Channel.prototype = prototype;
+var prototype = {
+  publish: function(message) {
+    var formattedMessage = this.name + ": " + message.content;
+
+    this.subscribers.forEach(function(subscriber) {
+      if(subscriber !== message.publisher) {
+        subscriber.send(formattedMessage);
+      }
+    });
+
+    return this;
+  },
+
+  addSubscriber: function(subscriber) {
+    this.subscribers.push(subscriber);
+  },
+
+  hasSubscriber: function(subscriber) {
+    return this.subscribers.indexOf(subscriber) > 0;
+  }
+};
+
 exports.Channel = Channel;
