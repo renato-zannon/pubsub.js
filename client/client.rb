ec6692d600604d44f1a4481add003e8c1d094716
Renato Riccieri <renato.riccieri@gmail.com>
2012-05-02 20:17:45 -0300

    Improved clients

--- client/client.rb
+++ client/client.rb
@@ -4,24 +4,28 @@ require "socket"
 socket = UDPSocket.new
 socket.bind("127.0.0.1", 0)
 
-subscription_message = "SUB test_channel"
+channel_name = ARGV.shift
+
+subscription_message = "SUB #{channel_name}"
 socket.send(subscription_message, 0, "127.0.0.1", 12345)
 
 Thread.new do
   loop do
     message, * = socket.recvfrom(1024)
-    puts message
+    puts "\n#{message}"
+    print "#{channel_name} >"
   end
 end
 
 loop do
+  print "#{channel_name} >"
   content = gets.chomp
 
   if content == "LIST"
     message = "LIST"
   else
     message = <<-MSG
-PUB test_channel
+PUB #{channel_name}
 #{content}
 ENDPUB
   MSG
---
+++ client/spammer.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+require "socket"
+
+socket = UDPSocket.new
+socket.bind("127.0.0.1", 0)
+
+channel_name = ARGV.shift
+content = ARGV.shift
+
+  message = <<-MSG
+PUB #{channel_name}
+#{content}
+ENDPUB
+  MSG
+
+loop do
+  sleep(0.1)
+  socket.send(message, 0, "127.0.0.1", 12345)
+end
