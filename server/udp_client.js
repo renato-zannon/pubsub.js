f02302333459e040900a497dfae9f4e815c54ff2
Renato Riccieri <renato.riccieri@gmail.com>
2012-05-10 01:29:22 -0300

    Simplified Client

--- server/udp_client.js
+++ server/udp_client.js
@@ -4,51 +4,26 @@ var Buffer = require('buffer').Buffer;
 var EventEmitter = require("events").EventEmitter;
 
 function Client(address, port) {
-  this._address = address;
-  this._port    = port;
+  this.address = address;
+  this.port    = port;
+  this.socket  = dgram.createSocket("udp4");
 
   this.socket.on("message", function(message) {
     this.emit("message", message);
   });
 };
 
-Client.prototype = Object.create(EventEmitter.prototype, {
-  "address": {
-    get: function() {
-      return this._address;
-    },
-    enumerable: true
-  },
+util.inherits(Client, EventEmitter);
 
-  "port": {
-    get: function() {
-      return this._port;
-    },
-    enumerable: true
-  },
-
-  "socket": {
-    get: function() {
-      this._socket = this._socket || dgram.createSocket("udp4");
-      return this._socket;
-    },
-    enumerable: true
-  },
-
-  "send": {
-    value: function(message, callback) {
-      if(!(message instanceof Buffer)) {
-        message = new Buffer(message);
-      }
-      this.socket.send(message, 0, message.length, this.port, this.address, callback);
-    }
-  },
-
-  "toString": {
-    value: function() {
-      return util.format("%s:%d", this.address, this.port);
-    }
+Client.prototype.send = function(message, callback) {
+  if(!(message instanceof Buffer)) {
+    message = new Buffer(message);
   }
-});
+  this.socket.send(message, 0, message.length, this.port, this.address, callback);
+};
+
+Client.prototype.toString = function() {
+  return util.format("%s:%d", this.address, this.port);
+};
 
 exports.Client = Client;
